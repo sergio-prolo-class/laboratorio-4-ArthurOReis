56957c71051abf941804a97b1a8b18a5c6236615
ArthurOReis <arthuroreis2005@gmail.com>
2025-06-06 16:56:14 -0300

    Criando classes herdeiras de Funcionario: CLT, Estágio e Temporário

---
+++ app/src/main/java/ifsc/poo/Lab_Classes/ClassesFuncionario/CLT.java
@@ -0,0 +1,11 @@
+package ifsc.poo.Lab_Classes.ClassesFuncionario;
+
+import ifsc.poo.Lab_Classes.Funcionario;
+
+public class CLT extends Funcionario {
+    
+    public CLT(String nome, String sobrenome, String cpf, String email, String dataInicioContrato){
+        super(nome, sobrenome, cpf, email, dataInicioContrato);
+    }
+
+}
---
+++ app/src/main/java/ifsc/poo/Lab_Classes/ClassesFuncionario/Temporario.java
@@ -0,0 +1,11 @@
+package ifsc.poo.Lab_Classes.ClassesFuncionario;
+
+import ifsc.poo.Lab_Classes.Funcionario;
+
+public class Temporario extends Funcionario {
+    
+    public Temporario(String nome, String sobrenome, String cpf, String email, String dataInicioContrato){
+        super(nome, sobrenome, cpf, email, dataInicioContrato);
+    }
+
+}
