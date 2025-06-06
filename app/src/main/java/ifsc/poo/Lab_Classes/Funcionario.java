676021f3ce68f13e2b81af9a44f2c24fe16c658d
ArthurOReis <arthuroreis2005@gmail.com>
2025-06-06 16:53:02 -0300

    Criando atributos e métodos para Funcionario

--- app/src/main/java/ifsc/poo/Lab_Classes/Funcionario.java
+++ app/src/main/java/ifsc/poo/Lab_Classes/Funcionario.java
@@ -1,9 +1,26 @@
 package ifsc.poo.Lab_Classes;
 
 public class Funcionario extends Pessoa {
+    private static int proximoIdentificador = 1;
+    private final int identificador;
+    private String dataInicioContrato;
 
-    public Funcionario(String nome, String sobrenome, String cpf, String email){
+    public Funcionario(String nome, String sobrenome, String cpf, String email, String dataInicioContrato){
         super(nome, sobrenome, cpf, email);
+        this.dataInicioContrato = dataInicioContrato;
+        this.identificador = proximoIdentificador++;
     }
-    
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public String getDataInicioContrato() {
+        return dataInicioContrato;
+    }
+
+    public void setDataInicioContrato(String dataInicioContrato) {
+        this.dataInicioContrato = dataInicioContrato;
+    }
+
 }
