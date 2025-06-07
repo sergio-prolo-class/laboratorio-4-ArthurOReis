76d15ae507bbfc8f1b9bce4e6ab761ca1932b035
ArthurOReis <arthuroreis2005@gmail.com>
2025-06-07 16:00:58 -0300

    Adicionando atributos de classes herdeiras de Estagiário (TI e Administração)

--- app/src/main/java/ifsc/poo/Lab_Classes/ClassesFuncionario/ClassesEstagiario/Administracao.java
+++ app/src/main/java/ifsc/poo/Lab_Classes/ClassesFuncionario/ClassesEstagiario/Administracao.java
@@ -4,9 +4,29 @@ import ifsc.poo.Lab_Classes.ClassesFuncionario.CLT;
 import ifsc.poo.Lab_Classes.ClassesFuncionario.Estagiario;
 
 public class Administracao extends Estagiario {
+    private String setorAtual;
+    private String descricaoTarefaPrincipal;
 
-    public Administracao(String nome, String sobrenome, String cpf, String email, String dataInicioContrato, double salarioBrutoMensal, int cargaHorariaSemanal, String nomeDaInstituicaoDeEnsino, CLT orientador){
+    public Administracao(String nome, String sobrenome, String cpf, String email, String dataInicioContrato, double salarioBrutoMensal, int cargaHorariaSemanal, String nomeDaInstituicaoDeEnsino, CLT orientador, String setorAtual, String descricaoTarefaPrincipal){
         super(nomeDaInstituicaoDeEnsino, nomeDaInstituicaoDeEnsino, nomeDaInstituicaoDeEnsino, nomeDaInstituicaoDeEnsino, nomeDaInstituicaoDeEnsino, cargaHorariaSemanal, cargaHorariaSemanal, nomeDaInstituicaoDeEnsino, orientador);
+        this.setorAtual = setorAtual;
+        this.descricaoTarefaPrincipal = descricaoTarefaPrincipal;
+    }
+    
+    public String getSetorAtual() {
+        return setorAtual;
+    }
+
+    public void setSetorAtual(String setorAtual) {
+        this.setorAtual = setorAtual;
+    }
+
+    public String getDescricaoTarefaPrincipal() {
+        return descricaoTarefaPrincipal;
+    }
+
+    public void setDescricaoTarefaPrincipal(String descricaoTarefaPrincipal) {
+        this.descricaoTarefaPrincipal = descricaoTarefaPrincipal;
     }
     
 }
--- app/src/main/java/ifsc/poo/Lab_Classes/ClassesFuncionario/ClassesEstagiario/TI.java
+++ app/src/main/java/ifsc/poo/Lab_Classes/ClassesFuncionario/ClassesEstagiario/TI.java
@@ -4,9 +4,28 @@ import ifsc.poo.Lab_Classes.ClassesFuncionario.CLT;
 import ifsc.poo.Lab_Classes.ClassesFuncionario.Estagiario;
 
 public class TI extends Estagiario {
+    private String numeroDaSerieComputador;
+    private boolean atuacaoRemotaPresenca;
 
-    public TI(String nome, String sobrenome, String cpf, String email, String dataInicioContrato, double salarioBrutoMensal, int cargaHorariaSemanal, String nomeDaInstituicaoDeEnsino, CLT orientador){
+    public TI(String nome, String sobrenome, String cpf, String email, String dataInicioContrato, double salarioBrutoMensal, int cargaHorariaSemanal, String nomeDaInstituicaoDeEnsino, CLT orientador, String numeroDaSerieComputador, boolean atuacaoRemotaPresenca){
         super(nomeDaInstituicaoDeEnsino, nomeDaInstituicaoDeEnsino, nomeDaInstituicaoDeEnsino, nomeDaInstituicaoDeEnsino, nomeDaInstituicaoDeEnsino, cargaHorariaSemanal, cargaHorariaSemanal, nomeDaInstituicaoDeEnsino, orientador);
+        this.numeroDaSerieComputador = numeroDaSerieComputador;
+        this.atuacaoRemotaPresenca = atuacaoRemotaPresenca;
     }
-    
+
+    public String getNumeroDaSerieComputador() {
+        return numeroDaSerieComputador;
+    }
+
+    public void setNumeroDaSerieComputador(String numeroDaSerieComputador) {
+        this.numeroDaSerieComputador = numeroDaSerieComputador;
+    }
+
+    public boolean isAtuacaoRemotaPresenca() {
+        return atuacaoRemotaPresenca;
+    }
+
+    public void setAtuacaoRemotaPresenca(boolean atuacaoRemotaPresenca) {
+        this.atuacaoRemotaPresenca = atuacaoRemotaPresenca;
+    }
 }
