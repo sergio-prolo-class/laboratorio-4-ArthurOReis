01a81c782e001f6962dcc2c09c557bcc82fd412f
ArthurOReis <arthuroreis2005@gmail.com>
2025-06-07 17:12:41 -0300

    Adicionando atributos e métodos de classes herdeiras de Temporário (Técnica e Consultoria)

--- app/src/main/java/ifsc/poo/Lab_Classes/ClassesFuncionario/ClassesTemporario/Consultoria.java
+++ app/src/main/java/ifsc/poo/Lab_Classes/ClassesFuncionario/ClassesTemporario/Consultoria.java
@@ -1,11 +1,32 @@
 package ifsc.poo.Lab_Classes.ClassesFuncionario.ClassesTemporario;
 
+import ifsc.poo.Lab_Classes.ClassesFuncionario.ClassesCLT.Pleno;
 import ifsc.poo.Lab_Classes.ClassesFuncionario.Temporario;
 
 public class Consultoria extends Temporario {
+    private int numeroRelatoriosParaEntregar;
+    private Pleno intermediario;
 
-    public Consultoria(String nome, String sobrenome, String cpf, String email, String dataInicioContrato, String dataTerminoContrato, double valorTotalContrato) {
+    public Consultoria(String nome, String sobrenome, String cpf, String email, String dataInicioContrato, String dataTerminoContrato, double valorTotalContrato, int numeroRelatoriosParaEntregar, Pleno intermediario) {
         super(nome, sobrenome, cpf, email, dataInicioContrato, dataTerminoContrato, valorTotalContrato);
+        this.numeroRelatoriosParaEntregar = numeroRelatoriosParaEntregar;
+        this.intermediario = intermediario;
+    }
+    
+    public int getNumeroRelatoriosParaEntregar() {
+        return numeroRelatoriosParaEntregar;
+    }
+
+    public void setNumeroRelatoriosParaEntregar(int numeroRelatoriosParaEntregar) {
+        this.numeroRelatoriosParaEntregar = numeroRelatoriosParaEntregar;
+    }
+
+    public Pleno getIntermediario() {
+        return intermediario;
+    }
+
+    public void setIntermediario(Pleno intermediario) {
+        this.intermediario = intermediario;
     }
     
 }
--- app/src/main/java/ifsc/poo/Lab_Classes/ClassesFuncionario/ClassesTemporario/Tecnica.java
+++ app/src/main/java/ifsc/poo/Lab_Classes/ClassesFuncionario/ClassesTemporario/Tecnica.java
@@ -1,11 +1,34 @@
 package ifsc.poo.Lab_Classes.ClassesFuncionario.ClassesTemporario;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ifsc.poo.Lab_Classes.ClassesFuncionario.Temporario;
 
 public class Tecnica extends Temporario {
+    private boolean necessidadeEPIs;
+    private List<String> turnosDeTrabalho; 
 
-    public Tecnica(String nome, String sobrenome, String cpf, String email, String dataInicioContrato, String dataTerminoContrato, double valorTotalContrato) {
+    public Tecnica(String nome, String sobrenome, String cpf, String email, String dataInicioContrato, String dataTerminoContrato, double valorTotalContrato, boolean necessidadeEPIs, List<String> turnosDeTrabalho) {
         super(nome, sobrenome, cpf, email, dataInicioContrato, dataTerminoContrato, valorTotalContrato);
+        this.necessidadeEPIs = necessidadeEPIs;
+        this.turnosDeTrabalho = new ArrayList<>(turnosDeTrabalho);
+    }
+
+    public boolean isNecessidadeEPIs() {
+        return necessidadeEPIs;
+    }
+
+    public void setNecessidadeEPIs(boolean necessidadeEPIs) {
+        this.necessidadeEPIs = necessidadeEPIs;
+    }
+
+    public List<String> getTurnosDeTrabalho() {
+        return new ArrayList<>(turnosDeTrabalho);
+    }
+
+    public void setTurnosDeTrabalho(List<String> turnosDeTrabalho) {
+        this.turnosDeTrabalho = new ArrayList<>(turnosDeTrabalho);
     }
 
 }
